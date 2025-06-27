60b98b7dbc2bc40c76a88356577baabddae987ed
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2025-06-27 10:32:45 -0300

    maquetado del formulario

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import Titulo from "./components/Titulo";
+import Formulario from "./components/Formulario";
 
 function App() {
   return (
     <main>
       <Titulo/>
+      <Formulario/>
     </main>
   )
 }
--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,9 +1,27 @@
+import Form from "react-bootstrap/Form";
+
 const Formulario = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <section className="container d-flex justify-content-center">
+      <div className="border border-light-subtle px-3 rounded mt-3">
+        <Form className="my-4">
+          <Form.Group className="d-flex gap-3">
+            <Form.Label>Buscar por categoria:</Form.Label>
+            <Form.Select aria-label="defaultSelect">
+              <option>Elige una opcion</option>
+              <option value="Business">Business</option>
+              <option value="Entertainment">Entertainment</option>
+              <option value="General">General</option>
+              <option value="Health">Health</option>
+              <option value="Science">Science</option>
+              <option value="Sports">Sports</option>
+              <option value="Technology">Technology</option>
+            </Form.Select>
+          </Form.Group>
+        </Form>
+      </div>
+    </section>
+  );
 };
 
 export default Formulario;
