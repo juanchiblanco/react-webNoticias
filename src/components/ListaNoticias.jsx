4a1a04aa643e8cde4624397ecc046c5a8b134ca6
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2025-06-27 11:21:36 -0300

    maquetado completo

--- src/components/ListaNoticias.jsx
+++ src/components/ListaNoticias.jsx
@@ -4,14 +4,6 @@ const ListaNoticias = () => {
     return (
         <div className="row justify-content-center my-3 row-gap-4">
             <div className="col-12 col-md-4 col-lg-3"><Noticia/></div>
-            <div className="col-12 col-md-4 col-lg-3"><Noticia/></div>
-            <div className="col-12 col-md-4 col-lg-3"><Noticia/></div>
-            <div className="col-12 col-md-4 col-lg-3"><Noticia/></div>
-            <div className="col-12 col-md-4 col-lg-3"><Noticia/></div>
-            <div className="col-12 col-md-4 col-lg-3"><Noticia/></div>
-            <div className="col-12 col-md-4 col-lg-3"><Noticia/></div>
-            <div className="col-12 col-md-4 col-lg-3"><Noticia/></div>
-            <div className="col-12 col-md-4 col-lg-3"><Noticia/></div>
         </div>
     );
 };
