2b85d7dd200f3d98a0c88a5468822619dd413d68
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2025-07-01 19:47:27 -0300

    agregue condicional en array si no hay noticias

--- src/components/ListaNoticias.jsx
+++ src/components/ListaNoticias.jsx
@@ -1,13 +1,19 @@
 import Noticia from "./Noticia";
 
-const ListaNoticias = ({noticias}) => {
-    return (
-        <div className="row justify-content-center my-3 row-gap-4">
-            {
-                noticias.map((noticia, indice)=><div className="col-12 col-md-4 col-lg-3" key={indice}><Noticia noticia={noticia}/></div>)
-            }
-        </div>
-    );
+const ListaNoticias = ({ noticias }) => {
+  return (
+    <div className="row justify-content-center my-3 row-gap-4">
+      {noticias.lenght == 0 ? (
+        <p className="text-center">No hay noticias disponibles.</p>
+      ) : (
+        noticias.map((noticia, indice) => (
+          <div className="col-12 col-md-4 col-lg-3" key={indice}>
+            <Noticia noticia={noticia} />
+          </div>
+        ))
+      )}
+    </div>
+  );
 };
 
 export default ListaNoticias;
--- src/components/Noticia.jsx
+++ src/components/Noticia.jsx
@@ -4,11 +4,11 @@ import Button from "react-bootstrap/Button";
 const Noticia = ({noticia}) => {
   return (
     <Card className="h-100">
-      <Card.Header className="text-center shadow bg-light text-dark h-100">
-        <p className="fs-5 text-uppercase">{noticia.country}</p>
+      <Card.Header className="shadow bg-light text-dark h-100">
+        <p className="fs-5 text-uppercase text-start">{noticia.country}</p>
         <img src={noticia.image_url
 || "https://hsconsultinggroup.net/wp-content/themes/consultix/images/no-image-found-360x250.png"} alt={noticia.title} className="img-fluid"/>
-        <p className="lead">{noticia.title} </p>
+        <p className="lead text-center">{noticia.title} </p>
       </Card.Header>
       <Card.Body className="text-center border-top border-bottom border-light subtle h-100">
         <p className="text-truncate">{noticia.description}</p>
