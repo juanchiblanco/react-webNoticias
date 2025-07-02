57d7c6baf826c6f5bb82e360133187d759bd0628
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2025-07-01 23:36:54 -0300

    agregue el footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,16 @@ import Formulario from "./components/Formulario";
 
 function App() {
   return (
-    <main>
-      <Titulo/>
-      <Formulario/>
-    </main>
-  )
+    <>
+      <main>
+        <Titulo />
+        <Formulario />
+      </main>
+      <footer className="bg-dark-subtle text-light text-center py-3">
+        <p>&copy;Todos los derechos reservados</p>
+      </footer>
+    </>
+  );
 }
 
-export default App
+export default App;
